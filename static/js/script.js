cfe86514d18a4e6f49ba181976dbf37de6d5590e
Created unity Project <nrhymer@students.kennesaw.edu>
2020-04-16 12:50:37 -0400

    Initial commit

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+function ageInDays(){
+var birthyear = prompt('Hey, What year were you Born?');
+var ageInDayss = (2020 - birthyear) * 365;
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode('You are ' + ageInDayss + ' days old');
+h1.setAttribute( 'id', 'ageInDays');
+h1.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendChild(h1); 
+console.log(ageInDayss);
+}
+
+function reset(){
+document.getElementById('ageInDays').remove();
+}
