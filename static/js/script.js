5b7edec18e7e78377a49efa5264cfdd497e56f50
Created unity Project <nrhymer@students.kennesaw.edu>
2020-04-16 19:33:58 -0400

    Challenge 1 & 2

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,10 @@ console.log(ageInDayss);
 function reset(){
 document.getElementById('ageInDays').remove();
 }
+
+function generateCat(){
+var image = document.createElement('img');
+var div = document.getElementById('flex-cat-gen');
+image.src = "https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small";
+div.appendChild(image);
+}
