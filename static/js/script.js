43e6ec7c880dd9e604f1af7dc17cd04fa2672b78
Created unity Project <nrhymer@students.kennesaw.edu>
2020-04-17 17:50:22 -0400

    Challenge 3 Rock Paper Sicssors Game
    One bug getting the computers image. not causing major problem but must be debugged more

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+
+
+
 function ageInDays(){
 var birthyear = prompt('Hey, What year were you Born?');
 var ageInDayss = (2020 - birthyear) * 365;
@@ -19,3 +22,79 @@ var div = document.getElementById('flex-cat-gen');
 image.src = "https://api.thecatapi.com/api/images/get?format=src&type=gif&size=small";
 div.appendChild(image);
 }
+
+//rps challenge
+function rpsGame(yourChoice){
+var humanChoice, botChoice;
+humanChoice = yourChoice.id;
+botChoice = numberToChoice(randToRpsInt());
+console.log('computerChoice', botChoice)
+results = decideWinner(humanChoice,botChoice); //returns a array of possible endings ie [0,1] human lost | bot won
+message = finalMessage(results) // returns 'You Won' 'You Lost' 'You Tied'
+console.log(message);
+rpsFrontEnd(yourChoice.id, botChoice.id, message);
+}
+
+function randToRpsInt(){
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number){
+    return['Rock','Paper','Scissors'][number]
+}
+
+function decideWinner(yourChoice, computerChoice){
+    var rpsDatabase = {
+    'Rock':{'Scissors':1, 'Rock':0.5, 'Paper':0},
+    'Paper':{'Rock': 1, 'Paper':0.5, 'Scissors':0},
+    'Scissors':{'Paper': 1, 'Scissors':0.5, 'Rock':0}
+    };
+    
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+   
+    return [yourScore, computerScore];
+
+}
+// why computerScore isnt being checked?? i have to continue debugging !!
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0 ){
+        return {'message':'You Lost lol', 'color': 'red'};
+    }
+    else if (yourScore === 0.5){
+        return{'message': 'You Tied!', 'color': 'yellow'};
+    }
+    else{
+        return{'message': 'You Won!', 'color': 'green'};
+    }
+    
+}
+
+function rpsFrontEnd(humanImageChoice,botImageChoice,finalMessage){
+    var imagesDatabase ={
+        'Rock': document.getElementById('Rock').src,
+        'Paper': document.getElementById('Paper').src,
+        'Scissors':document.getElementById('Scissors').src
+    }
+
+    document.getElementById('Rock').remove();
+    document.getElementById('Paper').remove();
+    document.getElementById('Scissors').remove();
+
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37,50,233,1);'>"
+    messageDiv.innerHTML ="<h1 style = 'color: " + finalMessage['color'] + "; font-size:60px; padding: 30px; '>" + finalMessage['message'] + "</h1>"
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243,38,24,1);'>"
+
+document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+document.getElementById('flex-box-rps-div').appendChild(botDiv);
+
+
+
+}
+
+
