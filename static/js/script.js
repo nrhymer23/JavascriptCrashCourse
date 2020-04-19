3b6bea7aff5d9e2c683701b039985253d346f3b7
Created unity Project <nrhymer@students.kennesaw.edu>
2020-04-19 16:07:42 -0400

    Blackjack game part2
    added assets and Hit Function on js script

--- static/js/script.js
+++ static/js/script.js
@@ -144,3 +144,43 @@ function randomColors(){
         all_buttons[i].classList.add(choices[randomNumber]);
     }
 }
+
+//Challenge 5: Blackjack
+let blackjackGame = {
+'you':{'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
+'dealer':{'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
+};
+
+const YOU = blackjackGame['you']
+const DEALER =blackjackGame['dealer']
+
+const hitSound = new Audio('static/sounds/swish.m4a');
+
+//adding event listeners  better than using onclick in html
+document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
+document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
+
+function blackjackHit(){
+showCard(YOU);
+}
+
+function showCard(activePlayer){
+    let cardImage = document.createElement('img');
+cardImage.src = 'static/images/Q.png';
+document.querySelector(activePlayer['div']).appendChild(cardImage);
+hitSound.play();
+}
+
+function blackjackDeal(){
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img');
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img');
+ 
+    for (i=0; i < yourImages.length; i++) {
+        yourImages[i].remove();
+    }
+
+
+    for (i=0; i < dealerImages.length; i++) {
+        dealerImages[i].remove();
+    }
+}
